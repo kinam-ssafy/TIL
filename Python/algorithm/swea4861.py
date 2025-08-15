8aa85dcf118d670da6719c47412a078703bcbe4d
kkn98kkn <zizon0305@gmail.com>
2025-08-15 14:49:07 +0900

    0815_14:49

--- Python/algorithm/swea4861.py
+++ Python/algorithm/swea4861.py
@@ -30,7 +30,7 @@ for t in range(1, T+1):
         for a in range(N-M+1): #열 회문 검사
             mini_str = []
             for m in range(M):
-                mini_str += arr[a+m][i]
+                mini_str += arr[a+m][i] # 각 열에 대한 값을 하나씩 넣어줌
             if mini_str == mini_str[::-1]:
                 print(''.join(mini_str))
                 break
