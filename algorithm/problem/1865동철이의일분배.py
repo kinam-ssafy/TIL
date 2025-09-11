54717d7d16d7b5770efbf35af4efac13f06d03ba
kinam-ssafy <zizon0305@gmail.com>
2025-09-11 17:56:44 +0900

    algo2

--- algorithm/problem/1865동철이의일분배.py
+++ algorithm/problem/1865동철이의일분배.py
@@ -15,8 +15,32 @@ i번 직원이 j번 일을 하면 성공할 확률이 Pi, j이다.
 
 우리는 여러 방법 중에서 생길 수 있는 “주어진 일이 모두 성공할 확률”의 최댓값을 구하는 프로그램을 작성해야 한다.
 
+[입력]
+
+첫 번째 줄에 테스트 케이스의 수 T가 주어진다.
+
+각 테스트 케이스의 첫 번째 줄에는 하나의 정수 N(1 ≤ N ≤ 16)이 주어진다.
+
+다음 N개의 줄의 i번째 줄에는 N개의 정수 Pi, 1, … , i, N(0 ≤ Pi, j ≤ 100)이 주어진다.
+
+Pi, j는 i번 사람이 j번 일을 성공할 확률을 퍼센트 단위로 나타낸다.
+
+
+[출력]
+
+각 테스트 케이스마다 ‘#x’(x는 테스트케이스 번호를 의미하며 1부터 시작한다)를 출력하고,
+
+모든 일을 성공할 확률이 최대화될 때의 확률을 퍼센트 단위로 소수점 아래 7번째 자리에서 반올림하여 6번째까지 출력한다.
+
+
+[예제 풀이]
+
+첫번째 직원이 첫번째 일을 담당하고, 두번째 직원이 두번째 일을 담당하고, 세번째 직원이 세번째 일을 담당할 때
+
+모든 일을 성공할 확률이 최대가 되고 그 확률은 (0.13*0.7*1.0)*100 = 9.1%가 된다.
+
 '''
-def dfs(row, sum):
+def dfs(row, sum, N):
     global max_sum
 
     if row == N:
@@ -24,10 +48,13 @@ def dfs(row, sum):
         max_sum = max(max_sum, sum)
         return
     
+    if max_sum >= sum:
+        return
+    
     for col in range(N):
         if not visited[col]:
             visited[col] = 1
-            dfs(row + 1, sum * arr[row][col])
+            dfs(row + 1, sum * arr[row][col] / 100, N)
             visited[col] = 0
 
 
@@ -39,6 +66,6 @@ for t in range(1, T+1):
     arr = [list(map(int, input().split())) for _ in range(N)]
     max_sum = 0
     
-    dfs(0, 1)
-    print(f"#{t} {max_sum/100**(N-1)}")
+    dfs(0, 1, N)
+    print(f"#{t} {max_sum*100:.6f}")
     
