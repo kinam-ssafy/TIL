82fee011236a5225221cc6f3f52abb3cf2410f67
kinam-ssafy <zizon0305@gmail.com>
2025-10-30 17:44:58 +0900

    algo2

--- Python/algorithm/boj/10_5th/boj14466.py
+++ Python/algorithm/boj/10_5th/boj14466.py
@@ -10,5 +10,41 @@
 K마리의 (1 ≤ K ≤ 100,K ≤ N2) 소가 존의 농장에 있고, 각 소는 서로 다른 목초지에 있다. 
 어떤 두 소는 길을 건너지 않으면 만나지 못 할 수 있다. 이런 소가 몇 쌍인지 세어보자.
 '''
+# 기본 NxN 배열은 visited처럼 0값 
+# r, c, r', c' 에 해당하는 인덱스를 1로
+# 1에서 1로는 못감 길이 있어서
+# 이 조건을 통해 BFS수행
+# 각 소에 대해서 BFS 수행하고 못찾는 소가 있으면 cnt + 1
+
+import sys
+from collections import deque
+input = sys.stdin.readline
 
 N, K, R = map(int, input().split())
+
+arr = [[0] * N for _ in range(N)]
+
+for _ in range(R):
+    r, c, r_dash, c_dash = map(int, input().split())
+    arr[c][r] = 1
+    arr[c_dash][r_dash] = 1
+
+cow_position = []
+for _ in range(K):
+    i, j = map(int, input().split())
+    cow_position.append((i, j))
+
+di = [0, 1, 0, -1]
+dj = [1, 0, -1, 0]
+
+def bfs(start_i, start_j):
+    q = deque([(start_i, start_j)])
+
+    while q:
+        ci, cj = q.popleft()
+
+        for d in range(4):
+            ni, nj = ci + d
+
+for i, j in cow_position:
+    bfs(i, j)
