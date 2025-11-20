6d7cd937104147963e0dbc924c707d33b6044a55
kkn98kkn <zizon0305@gmail.com>
2025-11-21 07:37:33 +0900

    C++

---
+++ C++/algorithm/11_3rd/boj1764.cpp
@@ -0,0 +1,25 @@
+/*boj1764 - 듣보잡
+
+김진영이 듣도 못한 사람의 명단과, 보도 못한 사람의 명단이 주어질 때, 
+듣도 보도 못한 사람의 명단을 구하는 프로그램을 작성하시오.
+*/
+
+/*
+C++의 리스트 : vector
+자료형 하나만 가능 : vector<int>는 정수형만 담음
+데이터 그 자체를 저장 : 리스트는 주소 저장
+len = size()
+.append(값) = push_back(값)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+
+
+    return 0;
+}
