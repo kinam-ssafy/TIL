8d2550130d67d467a1daa33187c2fac3a793751a
kinam-ssafy <zizon0305@gmail.com>
2026-01-07 17:48:28 +0900

    dp

--- C++/algorithm/dp/boj1463.cpp
+++ C++/algorithm/dp/boj1463.cpp
@@ -7,6 +7,34 @@ X가 3으로 나누어 떨어지면, 3으로 나눈다.
 X가 2로 나누어 떨어지면, 2로 나눈다.
 1을 뺀다.
 정수 N이 주어졌을 때, 위와 같은 연산 세 개를 적절히 사용해서 1을 만들려고 한다. 연산을 사용하는 횟수의 최솟값을 출력하시오.
-
-
 */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    int ans = 0;
+
+
+    while (n != 1) {
+        if (n % 3 == 0) {
+            n /= 3;
+        }
+        else {
+            n -= 1;
+        }
+
+        ans += 1;
+    }
+
+    cout << ans;
+
+    return 0;
+}
