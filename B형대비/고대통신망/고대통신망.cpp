286ecbcf5eaa597102af28f5a91e1da7662bbd5e
kinam <zizon0305@gmail.com>
2026-02-12 13:53:58 +0900

    algo

---
+++ B형대비/고대통신망/고대통신망.cpp
@@ -0,0 +1,25 @@
+
+
+
+void init(int N, int mCapital, int K, int mId[], int sCity[], int eCity[], int mDistance[]) {
+    // init(6, 1, 7, {2000,6000,5000,7000,1000,4000,3000}, {1,1,1,3,2,4,5}, {2,0,3,2,4,5,3}, {9,5,8,1,3,5,2})
+    
+
+    return;
+}
+
+void add(int mId, int sCity, int eCity, int mDistance) {
+
+
+    return;
+}
+
+void remove(int mId){
+
+    return;
+}
+
+int calculate(int mCity){
+
+    return 0; 
+}
