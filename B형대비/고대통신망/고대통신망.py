e450ccc48aa42f6f4785da659b8fc64da0429be7
kinam-ssafy <zizon0305@gmail.com>
2026-02-23 17:46:27 +0900

    정처기

---
+++ B형대비/고대통신망/고대통신망.py
@@ -0,0 +1,24 @@
+
+
+
+def init(N, mCapital, K, mId[], sCity[], eCity[], mDistance[]) 
+    # // init(6, 1, 7, {2000,6000,5000,7000,1000,4000,3000}, {1,1,1,3,2,4,5}, {2,0,3,2,4,5,3}, {9,5,8,1,3,5,2})
+    
+
+    return
+
+
+def add(mId, sCity, eCity, mDistance) 
+
+
+    return
+
+
+def remove(mId)
+
+    return
+
+
+def calculate(mCity)
+
+    return 0
